1eb5ae33f73345f6e85a4bfa8cb693cf0f553bb0
unknown <you@example.com>
2021-10-10 00:45:10 +0530

    made for bubble sort

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sorting_Visualizer
+
+This project is a Web Visualization tool for sorting algorithms.
+# need to add rest of sorting except bubble sort
+
---
+++ scripts/main.js
@@ -0,0 +1,91 @@
+
+//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
+var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
+var inp_gen=document.getElementById("a_generate");
+var inp_aspeed=document.getElementById("a_speed");
+//var array_speed=document.getElementById('a_speed').value;
+
+var butts_algos=document.querySelectorAll(".algos button");
+
+var div_sizes=[];
+var divs=[];
+let sp =[];
+var margin_size;
+var cont=document.getElementById("array_container");
+let disp = document.getElementById('array_disp');
+cont.style="flex-direction:row";
+
+//Array generation and updation.
+
+inp_gen.addEventListener("click",generate_array);
+inp_as.addEventListener("input",update_array_size);
+
+function generate_array()
+{
+    cont.innerHTML="";
+    disp.innerHTML='';
+    for(var i=0;i<array_size;i++)
+    {
+        div_sizes[i]=Math.floor(Math.random() * 0.5*(inp_as.max - inp_as.min) ) +40;
+        divs[i]=document.createElement("div");
+        sp[i]=document.createElement("small");
+        sp[i].innerHTML=div_sizes[i]+",";
+        sp[i].style.marginLeft="2px"
+        disp.appendChild(sp[i]);
+        
+        cont.appendChild(divs[i]);
+        margin_size=0.1;
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:pink; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+    }
+}
+
+function update_array_size()
+{
+    array_size=inp_as.value;
+    generate_array();
+}
+
+window.onload=update_array_size();
+
+//Running the appropriate algorithm.
+for(var i=0;i<butts_algos.length;i++)
+{
+    butts_algos[i].addEventListener("click",runalgo);
+}
+
+function disable_buttons()
+{
+    for(var i=0;i<butts_algos.length;i++)
+    {
+        butts_algos[i].classList=[];
+        butts_algos[i].classList.add("butt_locked");
+
+        butts_algos[i].disabled=true;
+        inp_as.disabled=true;
+        inp_gen.disabled=true;
+        inp_aspeed.disabled=true;
+    }
+}
+
+function runalgo()
+{
+    disable_buttons();
+
+    this.classList.add("butt_selected");
+    switch(this.innerHTML)
+    {
+        case "Bubble":Bubble();
+                        break;
+        case "Selection":Selection_sort();
+                        break;
+        case "Insertion":Insertion();
+                        break;
+        case "Merge":Merge();
+                        break;
+        case "Quick":Quick();
+                        break;
+        case "Heap":Heap();
+                        break;
+    }
+}
+
